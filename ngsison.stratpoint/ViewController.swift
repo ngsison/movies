69fba8b0f672eeb9fcd4f83792b0d0edb3a43769
Nathaniel Brion Sison <ngsison@outlook.com>
2018-08-14 19:27:49 +0800

    Initial commit

---
+++ ngsison.stratpoint/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ngsison.stratpoint
+//
+//  Created by Nathaniel SISON on 14/8/2018.
+//  Copyright © 2018 Nathaniel Brion Sison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
