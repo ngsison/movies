83b86608e494aab874c810fa21b8fb95ef3150fb
Nathaniel Brion Sison <ngsison@outlook.com>
2018-08-17 16:06:26 +0800

    Removed extra container in MovieTableViewCell

--- ngsison.stratpoint/Views/Table View Cells/MovieTableViewCell.swift
+++ ngsison.stratpoint/Views/Table View Cells/MovieTableViewCell.swift
@@ -7,9 +7,11 @@ class MovieTableViewCell: UITableViewCell {
     // MARK: - Properties
     static let identifier = "movieTableViewCell"
     
-    private lazy var container: UIView = {
-        let view = UIView()
-        return view
+    private lazy var backgroundImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = UIViewContentMode.scaleToFill
+        imageView.clipsToBounds = true
+        return imageView
     }()
     
     private lazy var titleLabel: UILabel = {
@@ -23,13 +25,6 @@ class MovieTableViewCell: UITableViewCell {
         let label = UILabel()
         label.textColor = UIColor.white
         return label
-    }()
-    
-    private lazy var backgroundImage: UIImageView = {
-        let imageView = UIImageView()
-        imageView.contentMode = UIViewContentMode.scaleToFill
-        imageView.clipsToBounds = true
-        return imageView
     }()
     
     
@@ -61,40 +56,31 @@ class MovieTableViewCell: UITableViewCell {
     
     // MARK: - Setup Views
     private func setupViews() {
-        setupContainer()
         setupBackgroundImage()
         setupYearLabel()
         setupTitleLabel()
     }
     
-    private func setupContainer() {
-        self.addSubview(container)
-        container.anchor(top: self.topAnchor, equalTo: 0)
-        container.anchor(left: self.leftAnchor, equalTo: 0)
-        container.anchor(bottom: self.bottomAnchor, equalTo: 0)
-        container.anchor(right: self.rightAnchor, equalTo: 0)
-    }
-    
     private func setupBackgroundImage() {
-        container.addSubview(backgroundImage)
-        backgroundImage.anchor(top: container.topAnchor, equalTo: 0)
-        backgroundImage.anchor(left: container.leftAnchor, equalTo: 0)
-        backgroundImage.anchor(bottom: container.bottomAnchor, equalTo: 0)
-        backgroundImage.anchor(right: container.rightAnchor, equalTo: 0)
+        self.addSubview(backgroundImage)
+        backgroundImage.anchor(top: self.topAnchor, equalTo: 0)
+        backgroundImage.anchor(left: self.leftAnchor, equalTo: 0)
+        backgroundImage.anchor(bottom: self.bottomAnchor, equalTo: 0)
+        backgroundImage.anchor(right: self.rightAnchor, equalTo: 0)
     }
     
     private func setupYearLabel() {
-        container.addSubview(yearLabel)
-        yearLabel.anchor(left: container.leftAnchor, equalTo: 15)
-        yearLabel.anchor(right: container.rightAnchor, equalTo: -15)
-        yearLabel.anchor(bottom: container.bottomAnchor, equalTo: -15)
+        self.addSubview(yearLabel)
+        yearLabel.anchor(left: self.leftAnchor, equalTo: 15)
+        yearLabel.anchor(right: self.rightAnchor, equalTo: -15)
+        yearLabel.anchor(bottom: self.bottomAnchor, equalTo: -15)
         yearLabel.anchor(height: 20)
     }
     
     private func setupTitleLabel() {
-        container.addSubview(titleLabel)
-        titleLabel.anchor(left: container.leftAnchor, equalTo: 15)
-        titleLabel.anchor(right: container.rightAnchor, equalTo: -15)
+        self.addSubview(titleLabel)
+        titleLabel.anchor(left: self.leftAnchor, equalTo: 15)
+        titleLabel.anchor(right: self.rightAnchor, equalTo: -15)
         titleLabel.anchor(bottom: yearLabel.topAnchor, equalTo: 0)
         titleLabel.anchor(height: 20)
     }
